8b1bea4821ac67b3d2c21e753054acb61473eaae
SphericPlanet <gauranshpandey09@gmail.com>
2024-09-05 19:19:31 +0530

    Created Number Swaping

---
+++ main.py
@@ -0,0 +1,12 @@
+x=int(input("Enter the digit of x: "))
+y=int(input("Enter the digit of y: "))
+
+print("Value of X before it got swaped",x)
+print("Value of Y before it got swaped",y)
+
+temp=x
+x=y
+y=temp
+
+print("Value of X after swapping",x)
+print("Value of Y after swaping",y)
